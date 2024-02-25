a885ef9665c45bd88667f2147514f8d09ca2a7bd
Jean-Marie Philocles <philocles.jeanmarie@gmail.com>
2024-02-25 10:27:03 +0100

    App - Screens folder structure

--- expointerim/App.js
+++ expointerim/App.js
@@ -4,7 +4,10 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>
+        Welcome to JMP's App!
+        <p>It is awesome</p>
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
