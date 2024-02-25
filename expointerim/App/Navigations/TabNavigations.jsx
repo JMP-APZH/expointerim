3ab1ed07c39721171fa0ee7dea507a6a0e4763f5
Jean-Marie Philocles <philocles.jeanmarie@gmail.com>
2024-02-25 16:41:55 +0100

    BottomNavigation preparation 1

---
+++ expointerim/App/Navigations/TabNavigations.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Tab = createBottomTabNavigator();
+
+export default function TabNavigations() {
+  return (
+    <View>
+      <Text>TabNavigations</Text>
+    </View>
+  )
+}
