e66027d1cbec37c7b6bfdc313ce88cd016a36fa4
Jean-Marie Philocles <philocles.jeanmarie@gmail.com>
2024-02-25 17:53:27 +0100

    BottomNavigation completed

--- expointerim/App/Navigations/TabNavigations.jsx
+++ expointerim/App/Navigations/TabNavigations.jsx
@@ -5,26 +5,67 @@ import HomeScreen from '../Screens/HomeScreen/HomeScreen';
 import ProfileScreen from '../Screens/ProfileScreen/ProfileScreen';
 import BookingScreen from '../Screens/BookingScreen/BookingScreen';
 
+import { FontAwesome, Ionicons  } from '@expo/vector-icons';
+import Colors from '../Utils/Colors';
+
+
 const Tab = createBottomTabNavigator();
 
 export default function TabNavigations() {
   return (
     <Tab.Navigator
         screenOptions={{
-            headerShown:false
+            headerShown:false,
+            tabBarActiveTintColor:Colors.PRIMARY
         }}
     >
       <Tab.Screen 
         name='home'
         component={HomeScreen}
+        options={{
+            tabBarLabel:({color}) => (
+                <Text 
+                    style={{color:color, fontSize:12, marginTop:-7}}
+                >
+                    Home
+                </Text>
+            ),
+            tabBarIcon:({color,size}) => (
+                <FontAwesome name="home" size={size} color={color} />
+            )
+        }}
       />
       <Tab.Screen 
         name='booking'
         component={BookingScreen}
+        options={{
+            tabBarLabel:({color}) => (
+                <Text 
+                    style={{color:color, fontSize:12, marginTop:-7}}
+                >
+                    Bookings
+                </Text>
+            ),
+            tabBarIcon:({color,size}) => (
+                <Ionicons name="bookmarks-sharp" size={size} color={color} />
+            )
+        }}
       />
       <Tab.Screen 
         name='profile'
         component={ProfileScreen}
+        options={{
+            tabBarLabel:({color}) => (
+                <Text 
+                    style={{color:color, fontSize:12, marginTop:-7}}
+                >
+                    Profile
+                </Text>
+            ),
+            tabBarIcon:({color,size}) => (
+                <FontAwesome name="user-circle" size={size} color={color} />
+            )
+        }}
       />
     </Tab.Navigator>
   )
