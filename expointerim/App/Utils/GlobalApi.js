4745fc9112bdffc73ca523bedc19a62fb7949859
Jean-Marie Philocles <philocles.jeanmarie@gmail.com>
2024-02-26 16:10:12 +0100

    HomeScreen - adding the slider - pprep - graphql (Hygraph) query

---
+++ expointerim/App/Utils/GlobalApi.js
@@ -0,0 +1,26 @@
+
+// API's Endpoints
+
+import { request, gql } from 'graphql-request'
+
+const M_URL = process.env.HYGRAPH_MASTER_URL
+
+const getSlider = async () => {
+
+    const query = gql`
+      query GetSlider {
+        sliders {
+            id
+            name
+            image {
+             url
+            }
+        }
+}
+    `
+    const result = await request(M_URL, query)
+
+    return result
+
+}
+ 
