93afe20fa03b1897d8fac8a4487d741acf52daaf
Jean-Marie Philocles <philocles.jeanmarie@gmail.com>
2024-02-27 17:30:58 +0100

    HomeScreen - BusinessList display prep

--- expointerim/App/Screens/HomeScreen/HomeScreen.jsx
+++ expointerim/App/Screens/HomeScreen/HomeScreen.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import Header from './Header'
 import Slider from './Slider'
 import Categories from './Categories'
+import BusinessList from './BusinessList'
 
 
 export default function HomeScreen() {
@@ -12,6 +13,7 @@ export default function HomeScreen() {
       <View style={{padding: 20}}>
         <Slider />
         <Categories />
+        <BusinessList />
       </View>
     </View>
   )
